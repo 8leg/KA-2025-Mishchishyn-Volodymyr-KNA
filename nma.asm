65e59b6752207da3d1c641d0c13c01a64dc6028c
8arm <vovkin122@gmail.com>
2025-03-24 20:05:54 +0200

    I don't really need getFile as a separate function do I? So now it's label

--- nma.asm
+++ nma.asm
@@ -2,18 +2,32 @@
 .data
     temp16 dw ?   ; used for 16-bit registry holding
     msg db '24', 0Dh, 0Ah, '$'
-    baseData db ?
+    fileData db 32768 dup(?)
     file db 20 dup(?)   ; stores the name of a file that our batch script gave us
 .code
 
 org 100h
 
 start:
+    lea di, file
+    mov si, 82h     ; 82 because there is two more useless symbols in memory as can be seen in dump
+getFilename:
+    mov ax, ds:[si] ; this line was killing me, but Claude told me more about ds indexing. Thanks mate
+    inc si
+	cmp al, 20h     ; those both set flags so why not do it like that?
+    cmp al, 0Dh     ; carriage return and space both deliminate (I know clever words) so yeah
+    je printRes
+    mov [di], ax
+    inc di
+    jmp getFilename
 
-    call getFile
+printRes:
     mov byte ptr [di], '$'
     lea si, file
     call print_message
+
+
+
 
 exit:
     mov ax, 4C00h
@@ -54,38 +68,5 @@ clearRegs proc
     xor si, si
     xor di, di
 clearRegs endp
-
-getToData proc
-    mov ah, 3dh
-    mov bx, 0h
-    mov cx, 1
-    mov dx, offset baseData
-    seekLoop:
-        int 21h
-        call saveRegs
-        mov cl, baseData
-        mov msg, cl
-        call print_message
-        call loadRegs
-        cmp baseData, 0h
-        je seekLoop
-    ret
-getToData endp
-
-getFile proc
-    lea di, file
-    mov si, 82h
-begin:
-    mov ax, ds:[si] ; this line was killing me for a lot of time, but Claude told me more about indexing. Thanks mate
-    inc si
-	cmp al, 20h ; those both set flags so why not do it like that?
-    cmp al, 0Dh ; carriage return and space both deliminate so yeah
-    je cont
-    mov [di], ax
-    inc di
-    jmp begin
-cont:
-    ret
-getFile endp
 
 end start
